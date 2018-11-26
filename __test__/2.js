5cc0ce4a5980e55a3240f7c878b7e7fb57be70d9
react5456 <2404780327@qq.com>
2018-11-26 09:57:56 +0800

    update

---
+++ __test__/2.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const os = require('os')
+const exec = require('child_process').exec
+const test = () => {
+  let cwd = process.cwd()
+  let homedir = os.homedir()
+  console.log('cwd', cwd, path.resolve(process.cwd()))
+  console.log('homedir', homedir)
+  process.chdir(`D:/2018-11-20`)
+  exec('mkdir test', (error, stdout, stderr) => {
+    if (error) {
+      console.log('error:', error)
+    } else {
+      console.log(stdout, stderr)
+    }
+  })
+}
+test()
